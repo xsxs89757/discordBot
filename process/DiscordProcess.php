3591bab647638e5e72f0e87f3a91087807140c95
wanglei <317448423@qq.com>
2023-04-19 15:24:03 +0800

    test

--- process/DiscordProcess.php
+++ process/DiscordProcess.php
@@ -22,13 +22,53 @@ class DiscordProcess
         
             // Listen for messages.
             $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+                $attachments_array = [];
+                $components_array = [];
+                if (count($message->attachments) > 0) {
+                    // 遍历所有附件
+                    foreach ($message->attachments as $attachment) {
+                        // 提取附件的相关信息
+                        $attachments_array[] = [
+                            'id' => $attachment->id,
+                            'filename' => $attachment->filename,
+                            'url' => $attachment->url,
+                            'proxy_url' => $attachment->proxy_url,
+                            'size' => $attachment->size,
+                            'height' => $attachment->height,
+                            'width' => $attachment->width
+                        ];
+                    }
+                }
+                if (count($message->components) > 0) {
+                    // 遍历所有组件行
+                    foreach ($message->components as $row) {
+                        $row_components = [];
+        
+                        // 遍历行内的每个组件
+                        foreach ($row as $component) {
+                            // 提取组件的相关信息
+                            $row_components[] = [
+                                'type' => $component->type,
+                                'style' => $component->style ?? null,
+                                'label' => $component->label ?? null,
+                                'emoji' => $component->emoji ?? null,
+                                'custom_id' => $component->custom_id ?? null,
+                                'url' => $component->url ?? null,
+                                'disabled' => $component->disabled ?? null,
+                            ];
+                        }
+        
+                        $components_array[] = $row_components;
+                    }
+                }
                 $data = [
                     'message' => $message->getRepositoryAttributes(),
                     'nonce' => $message->nonce,
-                    'attachments' => $message->attachments->toArray(),
-                    'components' => $message->components->toArray(),
+                    'attachments' => $attachments_array,
+                    'components' => $components_array,
                 ];
                 var_dump($data);
+                var_dump($message);
                 echo "{$message->author->username}: {$message->content}", PHP_EOL;
                 // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
             });
