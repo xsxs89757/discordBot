a933222bb809e84784d2af5fd3291e5aa69f76c2
wanglei <317448423@qq.com>
2023-04-19 17:35:19 +0800

    test

--- helpers/Sender.php
+++ helpers/Sender.php
@@ -2,6 +2,17 @@
 namespace helpers;
 
 use GuzzleHttp\Client;
+// $channelid = "1092701344449699841";
+//         $authorization = "MTA2NzA4MjU3NjU1ODMwNTI4MA.GhUKOG.tyJa1_F__yxsDiqyXox8g_EubtIdYye40M8u3U";
+//         $application_id = "936929561302675456";
+//         $guild_id = "1092701344009293895";
+//         $session_id = "9f905ea42829a92b5d86fa581ddd8d33";
+//         $version = "1077969938624553050";
+//         $id = "938956540159881230";
+//         $nonce = '223817256';
+//         $flags = "--v 5";
+//         $prompt = "forest, big bad wolf, unreal engine, cinematic lighting, UHD, super detail --aspect 2:3";
+//         Sender::getInstance($channelid, $authorization, $application_id, $guild_id, $session_id, $version, $id, $nonce, $flags)->send($prompt);
 
 class Sender
 {
@@ -15,8 +26,27 @@ class Sender
     private string $flags;
     private string $nonce;
 
+    const IMAGINE = 'imagine';
+    const FAST = 'fast';
+    const RELAX = 'relax';
+
+    const USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36';
+
     private static ?Sender $instance = null;
 
+    /**
+     * 构造函数
+     *
+     * @param string $channelid
+     * @param string $authorization
+     * @param string $application_id
+     * @param string $guild_id
+     * @param string $session_id
+     * @param string $version
+     * @param string $id
+     * @param string $nonce
+     * @param string $flags
+     */
     private function __construct(
         string $channelid,
         string $authorization,
@@ -39,6 +69,20 @@ class Sender
         $this->flags = $flags;
     }
 
+    /**
+     * 单例
+     *
+     * @param string $channelid
+     * @param string $authorization
+     * @param string $application_id
+     * @param string $guild_id
+     * @param string $session_id
+     * @param string $version
+     * @param string $id
+     * @param string $nonce
+     * @param string $flags
+     * @return Sender
+     */
     public static function getInstance(
         string $channelid,
         string $authorization,
@@ -57,10 +101,46 @@ class Sender
         return self::$instance;
     }
 
-    public function send(string $prompt): void
+    /**
+     * 切换fast状态
+     *
+     * @return string
+     */
+    public function switchModeFast() : string {
+        return $this->request(self::FAST);
+    }
+
+    /**
+     * 切换relax状态
+     *
+     * @return string
+     */
+    public function switchModeRelax() : string {
+        return $this->request(self::RELAX);
+    }
+
+    /**
+     * 发送消息
+     *
+     * @param string $prompt
+     * @return string
+     */
+    public function send(string $prompt): string
     {
+        return $this->request(self::IMAGINE, $prompt);
+    }
+
+    /**
+     * todo:发送消息 
+     *
+     * @param string $type
+     * @param string|null $prompt
+     * @return string
+     */
+    private function request(string $type, string $prompt = null) : string{
         $header = [
             'authorization' => $this->authorization,
+            'user-agent' => self::USER_AGENT
         ];
 
         $prompt = str_replace('_', ' ', $prompt);
@@ -77,9 +157,11 @@ class Sender
             'data' => [
                 'version' => $this->version,
                 'id' => $this->id,
-                'name' => 'imagine',
+                'name' => $type,
                 'type' => 1,
-                'options' => [['type' => 3, 'name' => 'prompt', 'value' => $prompt . ' ' . $this->flags]],
+                'options' => $type === 'imagine' ? [
+                    ['type' => 3, 'name' => 'prompt', 'value' => $prompt . ' ' . $this->flags]
+                ]: [],
                 'attachments' => [],
             ],
             'nonce' => $this->nonce,
@@ -94,6 +176,6 @@ class Sender
             $response = $client->post('https://discord.com/api/v9/interactions', ['json' => $payload]);
         } while ($response->getStatusCode() != 204);
 
-        echo "prompt [{$prompt}] successfully sent!\n";
+        return $type === 'imagine' ? $prompt  . ' ' . $this->flags : '';
     }
 }
--- process/DiscordProcess.php
+++ process/DiscordProcess.php
@@ -5,47 +5,91 @@ use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Discord\WebSockets\Intents;
 use Discord\WebSockets\Event;
-use helpers\Sender;
 
 class DiscordProcess
 {
     public function onWorkerStart()
     {
-        $channelid = "1092701344449699841";
-        $authorization = "MTA2NzA4MjU3NjU1ODMwNTI4MA.GhUKOG.tyJa1_F__yxsDiqyXox8g_EubtIdYye40M8u3U";
-        $application_id = "936929561302675456";
-        $guild_id = "1092701344009293895";
-        $session_id = "9f905ea42829a92b5d86fa581ddd8d33";
-        $version = "1077969938624553050";
-        $id = "938956540159881230";
-        $nonce = '223817256';
-        $flags = "--v 5";
-        $prompt = "forest, big bad wolf, unreal engine, cinematic lighting, UHD, super detail --aspect 2:3";
-        Sender::getInstance($channelid, $authorization, $application_id, $guild_id, $session_id, $version, $id, $nonce, $flags)->send($prompt);
+        $discord = new Discord([
+            'token' => config('env.discord.token'),
+            'intents' => Intents::getDefaultIntents(),
+        ]);
+        
+        $discord->on('ready', function (Discord $discord) {
+            echo "Bot is ready!", PHP_EOL;
+        
+            // Listen for messages.
+            $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+                $attachments_array = [];
+                $components_array = [];
+                if (count($message->attachments) > 0) {
+                    // 遍历所有附件
+                    foreach ($message->attachments as $attachment) {
+                        // 提取附件的相关信息
+                        $attachments_array[] = [
+                            'id' => $attachment->id,
+                            'filename' => $attachment->filename,
+                            'url' => $attachment->url,
+                            'proxy_url' => $attachment->proxy_url,
+                            'size' => $attachment->size,
+                            'height' => $attachment->height,
+                            'width' => $attachment->width
+                        ];
+                    }
+                }
+                if (count($message->components) > 0) {
+                    // 遍历所有组件行
+                    foreach ($message->components as $row) {
+                        $row_components = [];
+                        // 遍历行内的每个组件
+                        foreach ($row->components as $component) {
+                            // 提取组件的相关信息
+                            $row_components[] = [
+                                'type' => $component->type,
+                                'style' => $component->style ?? null,
+                                'label' => $component->label ?? null,
+                                'emoji' => $component->emoji ? ($component->emoji->name) : null,
+                                'custom_id' => $component->custom_id ?? null,
+                                'url' => $component->url ?? null,
+                                'disabled' => $component->disabled ?? null,
+                            ];
+                        }
+        
+                        $components_array[] = $row_components;
+                    }
+                }
+                $data = [
+                    'message' => $message->getRepositoryAttributes(),
+                    'nonce' => $message->nonce,
+                    'attachments' => $attachments_array,
+                    'components' => $components_array,
+                ];
+                var_dump($data);
+                echo "{$message->author->username}: {$message->content}", PHP_EOL;
+                // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+            });
+            $discord->on(Event::MESSAGE_UPDATE, function (Message $message, Discord $discord) {
+                if($message->author->username === 'Midjourney Bot'){
+                    $content = $message->content;
+                    preg_match('/\((\d{1,3}%)\)/', $content, $percentage);
+                    $percentage = $percentage[1]; //获取%比
+                    if($percentage){
+                        $data = [
+                            'message' => $message->getRepositoryAttributes(),
+                            'nonce' => $message->nonce,
+                            'progress' => $percentage
+                        ];
+                        var_dump($data);
+                    }
+                    
+                }
+                echo "{$message->author->username}: {$message->content}", PHP_EOL;
+                // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+            });
 
-        // $discord = new Discord([
-        //     'token' => config('env.discord.token'),
-        //     'intents' => Intents::getDefaultIntents(),
-        // ]);
+        });
         
-        // $discord->on('ready', function (Discord $discord) {
-        //     echo "Bot is ready!", PHP_EOL;
-        
-        //     // Listen for messages.
-        //     $discord->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
-        //         var_dump($message);
-        //         echo "{$message->author->username}: {$message->content}", PHP_EOL;
-        //         // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
-        //     });
-        //     $discord->on(Event::MESSAGE_UPDATE, function (Message $message, Discord $discord) {
-        //         var_dump($message);
-        //         echo "{$message->author->username}: {$message->content}", PHP_EOL;
-        //         // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
-        //     });
-
-        // });
-        
-        // $discord->run();
+        $discord->run();
     }
 
 }
