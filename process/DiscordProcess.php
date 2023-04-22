875101f951b0a4fea4d70d95a500ba169d9b8025
wanglei <317448423@qq.com>
2023-04-22 14:13:43 +0800

    test

--- process/DiscordProcess.php
+++ process/DiscordProcess.php
@@ -29,7 +29,7 @@ class DiscordProcess
                             $compressedData = $msg->getPayload();
 
                             // 解压缩数据
-                            $uncompressedData = zlib_decode($compressedData);
+                            $uncompressedData = gzuncompress($compressedData);
                 
                             // 检查解压缩是否成功
                             if ($uncompressedData === false) {
